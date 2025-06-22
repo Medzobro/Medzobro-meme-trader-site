3b699ec52f619ebd7c512bcd34f66e2a666eb498
Medzobro <162759408+Medzobro@users.noreply.github.com>
2025-06-22 17:44:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function copyAddress() {
-  const contract = "0x616A1de200d49e210de940F91FB636f67b75d657"; // ضع هنا عنوان العقد الحقيقي
+  const contract = "0x616A1de200d49e210de940F91FB636f67b75d656";
   navigator.clipboard.writeText(contract).then(() => {
     const copiedMsg = document.getElementById("copied");
     copiedMsg.classList.remove("hidden");
