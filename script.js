abd6f09d0a05d7ce3609dff5ab8c72e5ec6ef148
Medzobro <162759408+Medzobro@users.noreply.github.com>
2025-06-22 17:39:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function copyAddress() {
-  const contract = "0x616A1de200d49e210de940F91FB636f67b75d657"; // استبدل بالعنوان الحقيقي
+  const contract = "0x616A1de200d49e210de940F91FB636f67b75d657"; // ضع هنا عنوان العقد الحقيقي
   navigator.clipboard.writeText(contract).then(() => {
-    document.getElementById("copied").classList.remove("hidden");
+    const copiedMsg = document.getElementById("copied");
+    copiedMsg.classList.remove("hidden");
     setTimeout(() => {
-      document.getElementById("copied").classList.add("hidden");
+      copiedMsg.classList.add("hidden");
     }, 3000);
   });
 }
